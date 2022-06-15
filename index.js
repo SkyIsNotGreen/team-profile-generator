48b3506917110e3f51863cf3e68a4b78d84f62da
Ricky Palmer <r.palmer@outlook.com>
2022-06-15 18:37:53 +0100

    added JS boiler plate

---
+++ index.js
@@ -0,0 +1,9 @@
+// dependencies 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const createHTML = require('./src/create-html.js')
+
+const Employee = require('./lib/Employee.js')
+const Manager = require('./lib/Manager.js')
+const Intern = require('./lib/Intern.js')
+const Engineer = require('./lib/Engineer.js');
---
+++ src/html-creator.js
@@ -0,0 +1,9 @@
+// dependencies
+const employee = require("../lib/employee.js")
+const manager = require("../lib/manager.js")
+const intern = require("../lib/intern.js")
+const engineer = require("../lib/engineer.js");
+
+
+
+module.exports = createHTML;
