d094c79aa09ee510cbb1c2e0778e6f138957ae94
Ricky Palmer <r.palmer@outlook.com>
2022-06-17 20:21:55 +0100

    created main app logic

--- index.js
+++ index.js
@@ -1,67 +1,132 @@
-// dependencies 
-const inquirer = require('inquirer');
-const fs = require('fs');
-const createHTML = require('./src/create-html.js')
+// dependencies
+const inquirer = require("inquirer");
+const fs = require("fs");
+const createHTML = require("./src/create-html.js");
 
-const Employee = require('./lib/Employee.js')
-const Manager = require('./lib/Manager.js')
-const Intern = require('./lib/Intern.js')
-const Engineer = require('./lib/Engineer.js');
+const Employee = require("./lib/employee.js");
+const Manager = require("./lib/manager.js");
+const Intern = require("./lib/intern.js");
+const Engineer = require("./lib/engineer.js");
 
 const staff = [];
 
 // function to get user input and create new employee object
 const getEmployeeInfo = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: `What is the ${role}'s name?`,
+        name: "name",
+      },
 
-    return inquirer.prompt([
+      {
+        type: "number",
+        message: `What is the ${role}'s ID?`,
+        name: "id",
+      },
 
-        {
-            type: "input",
-            message: `What is the ${role}'s name?`,
-            name: "name",
-        },
+      {
+        type: "input",
+        message: `What is the ${role}'s email?`,
+        name: "email",
+      },
 
-        {
-            type: "number",
-            message: `What is the ${role}'s ID?`,
-            name: "id",
-        },
+      {
+        type: "input",
+        message: "What is the manager's office location?",
+        name: "officeLocation",
+        when: role === "manager",
+      },
 
-        {
-            type: "input",
-            message: `What is the ${role}'s email?`,
-            name: "email",
-        },
+      {
+        type: "input",
+        message: "What is the engineer's GitHub username?",
+        name: "github",
+        when: role === "engineer",
+      },
 
-        {
-            type: "input",
-            message: "What is the manager's office location?",
-            name: "officeLocation",
-            when: role === 'manager'
-        },
+      {
+        type: "input",
+        message: "What is the intern's school?",
+        name: "school",
+        when: role === "intern",
+      },
 
-        {
-            type: "input",
-            message: "What is the engineer's GitHub username?",
-            name: "github",
-            when: role === "engineer"
-        },
+      {
+        type: list,
+        message: "What do you wish to do next?",
+        name: "nextStep",
+        choices: ["Add an engineer", "Add an intern", "Build my team"],
+      },
+    ])
 
-        {
-            type: "input",
-            message: "What is the intern's school?",
-            name: "school",
-            when: role === "intern"
-        }, 
+    .then((response) => {
+      let newEmployee = {};
 
-        {
-            type: list,
-            message: "What do you wish to do next?",
-            name: "nextStep",
-            choices: ["Add an engineer", "Add an intern", "Build my team"]
-        }
-    ]);
+      switch (role) {
+        case "manager":
+          newEmployee = new Manager(
+            response.name,
+            response.id,
+            response.email,
+            response.officeLocation
+          );
+          break;
 
-    
+        case "engineer":
+          newEmployee = new Engineer(
+            response.name,
+            response.id,
+            response.email,
+            response.github
+          );
+          break;
 
-}
+        case "intern":
+          newEmployee = new Intern(
+            response.name,
+            response.id,
+            response.email,
+            response.school
+          );
+          break;
+
+        default:
+          throw new Error("Something went wrong");
+      }
+
+      // add new employee to staff array
+      staff.push(newEmployee);
+
+      if (response.nextStep === "Add an engineer") {
+        return getEmployeeInfo("engineer");
+
+      } else if (response.nextStep === "Add an intern") {
+        return getEmployeeInfo("intern");
+
+      } else if (response.nextStep === "Build my team") {
+        const html = createHTML(staff);
+        writeToFile(html);
+        
+      } else {
+        throw new Error("Something went wrong");
+      }
+    });
+};
+
+// write html file
+const writeToFile = (generatedHTML) => {
+  fs.writeFile("./dist/team.html", generatedHTML, (err) => {
+    if (err) throw err;
+    console.log("Successfully wrote to team.html");
+  });
+};
+
+const init = () => {
+    console.log("Welcome to the team generator!");
+    console.log("Please select a role to begin.");
+    getEmployeeInfo();
+};
+
+init ();
