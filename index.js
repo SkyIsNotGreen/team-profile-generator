ef7a2a38a932c55c476dc76e2ba48f776dfe0d1e
Ricky Palmer <r.palmer@outlook.com>
2022-06-17 18:48:53 +0100

    added class roles and questions

--- index.js
+++ index.js
@@ -7,3 +7,61 @@ const Employee = require('./lib/Employee.js')
 const Manager = require('./lib/Manager.js')
 const Intern = require('./lib/Intern.js')
 const Engineer = require('./lib/Engineer.js');
+
+const staff = [];
+
+// function to get user input and create new employee object
+const getEmployeeInfo = () => {
+
+    return inquirer.prompt([
+
+        {
+            type: "input",
+            message: `What is the ${role}'s name?`,
+            name: "name",
+        },
+
+        {
+            type: "number",
+            message: `What is the ${role}'s ID?`,
+            name: "id",
+        },
+
+        {
+            type: "input",
+            message: `What is the ${role}'s email?`,
+            name: "email",
+        },
+
+        {
+            type: "input",
+            message: "What is the manager's office location?",
+            name: "officeLocation",
+            when: role === 'manager'
+        },
+
+        {
+            type: "input",
+            message: "What is the engineer's GitHub username?",
+            name: "github",
+            when: role === "engineer"
+        },
+
+        {
+            type: "input",
+            message: "What is the intern's school?",
+            name: "school",
+            when: role === "intern"
+        }, 
+
+        {
+            type: list,
+            message: "What do you wish to do next?",
+            name: "nextStep",
+            choices: ["Add an engineer", "Add an intern", "Build my team"]
+        }
+    ]);
+
+    
+
+}
