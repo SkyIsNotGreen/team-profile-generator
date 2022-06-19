f427c9970ab868492877ff2f93930c1e0c312991
Ricky Palmer <r.palmer@outlook.com>
2022-06-19 18:11:08 +0100

    bug fixes for manager card

--- src/html-creator.js
+++ src/html-creator.js
@@ -17,7 +17,7 @@ const createManagerCard = (manager) => {
       <ul class="list-group">
         <li class="list-group-item">ID: ${manager.getId()}</li>
         <li class="list-group-item">Email: <a href="${manager.getEmail()}">${manager.getEmail()}</a></li>
-        <li class="list-group-item">Office: ${manager.officeNumber}</li>
+        <li class="list-group-item">Office: ${manager.officeLocation}</li>
       </ul>
     </div>
   </div>
