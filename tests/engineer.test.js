4157fdd4ea509890b3490954b2330c53353b044c
Ricky Palmer <r.palmer@outlook.com>
2022-06-19 16:54:28 +0100

    created rest of tests and bug fixes

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,1 +1,34 @@
-const engineer = require("../lib/engineer.js")
+const Engineer = require("../lib/engineer.js")
+
+// test to see if the engineer constructor is working correctly
+describe("Engineer", () => {
+    it("should create an engineer object", () => {
+       const engineer = new Engineer ("John", 123, "john@email.com", "john123");
+
+        expect(engineer.name).toEqual("John");
+        expect(engineer.id).toEqual(123);
+        expect(engineer.email).toEqual("john@email.com");
+        expect(engineer.github).toEqual("john123");
+    });
+});
+
+// test individual engineer properties
+
+describe("getGithub", () => {
+    it("should return the github username of the engineer", () => {
+        const engineer = new Engineer("Bob", 123, "john@email.com", "bob123");
+
+        expect(engineer.getGithub()).toEqual("bob123");
+    });
+});
+
+describe("getRole", () => {
+    it("should return the role of the engineer", () => {
+        const engineer = new Engineer("Peegor", 123, "peegor@email.com", "peegor123");
+
+        expect(engineer.getRole()).toEqual("engineer");
+    });
+});
+
+
+
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,31 @@
-const manager = require("../lib/manager.js")
+const Manager = require("../lib/manager.js")
+
+// test to see if the manager constructor is working correctly
+describe("Manager", () => {
+    it("should create a manager object", () => {
+       const manager = new Manager ("John", 123, "john@email.com", "Birmingham University");
+
+        expect(manager.name).toEqual("John");
+        expect(manager.id).toEqual(123);
+        expect(manager.email).toEqual("john@email.com");
+        expect(manager.officeLocation).toEqual("Birmingham University");
+    });
+});
+
+// test individual manager properties
+describe("getOfficeLocation", () => {
+    it("should return the office location of the manager", () => {
+        const manager = new Manager("Bob", 123, "bob@email.com", "Birmingham University");
+
+        expect(manager.getOfficeLocation()).toEqual("Birmingham University");
+    });
+});
+
+describe("getRole", () => {
+    it("should return the role of the manager", () => {
+        const manager = new Manager("Peegor", 123, "peegor@email.com", "Birmingham University");
+
+        expect(manager.getRole()).toEqual("manager");
+    });
+});
+
